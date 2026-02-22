8ff320d9e9e65f0a0e7fa205e38cd0652ea21794
Rafael Guzman <rega1237@gmail.com>
2026-02-22 11:48:17 -0400

    feat(integrations): setup google model-viewer and add admin preview layout

---
+++ vendor/javascript/@google--model-viewer.js
@@ -0,0 +1,18 @@
+// @google/model-viewer@4.1.0 downloaded from https://ga.jspm.io/npm:@google/model-viewer@4.1.0/lib/model-viewer.js
+
+import{AnimationMixin as e}from"./features/animation.js";import{AnnotationMixin as r}from"./features/annotation.js";import{ARMixin as s}from"./features/ar.js";import{S as t,E as o,C as m,M as i}from"../_/DNCEymZ0.js";import{LoadingMixin as a}from"./features/loading.js";import{StagingMixin as p}from"./features/staging.js";export{CanvasTexture,FileLoader,Loader,NearestFilter}from"three";import"lit/decorators.js";import"three/examples/jsm/exporters/USDZExporter.js";import"./constants.js";import"./styles/deserializers.js";import"./styles/parsers.js";import"./utilities.js";import"lit";import"./template.js";import"./three-components/CachingGLTFLoader.js";import"three/examples/jsm/libs/meshopt_decoder.module.js";import"three/examples/jsm/loaders/DRACOLoader.js";import"three/examples/jsm/loaders/GLTFLoader.js";import"three/examples/jsm/loaders/KTX2Loader.js";import"three/examples/jsm/renderers/CSS2DRenderer.js";import"three/examples/jsm/utils/SceneUtils.js";import"three/examples/jsm/exporters/GLTFExporter.js";import"three/examples/jsm/webxr/XREstimatedLight.js";import"../_/CspHyBD3.js";import"./styles/conversions.js";import"./utilities/animation.js";import"three/examples/jsm/utils/SkeletonUtils.js";import"@monogrid/gainmap-js";import"three/examples/jsm/loaders/RGBELoader.js";import"three/examples/jsm/shaders/HorizontalBlurShader.js";import"three/examples/jsm/shaders/VerticalBlurShader.js";import"three/src/math/MathUtils.js";import"./utilities/progress-tracker.js";
+/* @license
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */const j=r(t(p(o(m(s(a(e(i))))))));customElements.define("model-viewer",j);export{j as ModelViewerElement};
+
